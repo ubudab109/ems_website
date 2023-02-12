b9aa61e0ff8b6b2bc6211925478da0a7f9cc3aad
unknown <bramatrihadisyapoetra@gmail.com>
2023-02-13 02:25:35 +0700

    Fix Login Page

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,35 +1,35 @@
-import React, { Fragment, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { Fragment, useState } from "react";
+import { useDispatch } from "react-redux";
 import axios from "axios";
 import { withRouter } from "react-router";
-import { loginProcess } from '../../app/redux/reducer';
-import '../../assets/sass/Auth.scss';
-import { URL_SERVICE } from '../../utils/constant';
-import { setStore } from '../../utils/helper';
+import { loginProcess } from "../../app/redux/reducer";
+import "../../assets/sass/Auth.scss";
+import { URL_SERVICE } from "../../utils/constant";
+import { setStore } from "../../utils/helper";
 
 const Login = () => {
-  const [typePassword, setTypePassword] = useState('password');
-  const [credential, setCredential] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [typePassword, setTypePassword] = useState("password");
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const [submitted, setSubmitted] = useState(false);
   const dispatch = useDispatch();
 
   const visiblePasswordChange = () => {
-    if (typePassword === 'password') {
-      setTypePassword('text');
+    if (typePassword === "password") {
+      setTypePassword("text");
     } else {
-      setTypePassword('password');
+      setTypePassword("password");
     }
-  }
+  };
 
   const handleFormSubmit = async (event) => {
-    event.preventDefault()
-    setSubmitted(true)
+    event.preventDefault();
+    setSubmitted(true);
     try {
       const res = await axios.post(`${URL_SERVICE}login`, {
         credential: credential,
-        password: password
+        password: password,
       });
       const token = res.data.data.token;
       const data = res.data.data.user_data;
@@ -42,25 +42,25 @@ const Login = () => {
       } else {
         branchId = null;
       }
-      const isSuperAdmin = res.data.data.user_data.is_superadmin
+      const isSuperAdmin = res.data.data.user_data.is_superadmin;
       dispatch(loginProcess(data, role, permissions, isSuperAdmin, branch));
-      setStore('web-token', token);
-      setStore('branch-selected', branchId);
-      setSubmitted(false)
+      setStore("web-token", token);
+      setStore("branch-selected", branchId);
+      setSubmitted(false);
       window.location.reload();
     } catch (err) {
-      setError(err.response.data.message)
-      setSubmitted(false)
+      console.log(err)
+      setError(err.response.data.message);
+      setSubmitted(false);
     }
-
-  }
+  };
 
   return (
     <Fragment>
-      <div className="container">
-        <div className="row no-gutter">
-          <div className="col-xl-7 col-lg-12 col-md-12 col-sm-12 d-none d-md-flex">
-            <div className="col-xl-12 col-lg-6 col-md-6 col-sm-6 left-bg">
+      {/* <div className="container">
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="col-xl-6">
               <div className="title-wrap">
                 <h3 className="title-welcome">Selamat Datang</h3>
                 <h4 className="subtitle-welcome">Kelola Staf Anda Dengan Mudah</h4>
@@ -70,7 +70,7 @@ const Login = () => {
               </div>
             </div>
           </div>
-          <div className="col-xl-5 col-lg-12 col-md-12 col-sm-12 bg-blue-rounded">
+          <div className="col-xl-6 bg-blue-rounded">
             <div className="login">
               <div className="container-fluid">
                 <div className="row">
@@ -126,6 +126,94 @@ const Login = () => {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </div> */}
+      <div className="vh-100 w-100 row m-0">
+        <div className="col-lg-7 align-items-center p-5">
+          <div className="title-wrap mb-5">
+            <h3 className="title-welcome">Selamat Datang</h3>
+            <h4 className="subtitle-welcome">Kelola Staf Anda Dengan Mudah</h4>
+          </div>
+          <div className="w-100 d-lg-flex align-items-center justify-content-center px-5">
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/img/login_bg_left.png`}
+              alt=""
+              width="450"
+            />
+          </div>
+        </div>
+        <div className="d-flex col-lg-5 align-items-center auth-bg p-lg-5 bg-blue-rounded text-white">
+          <div className="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
+            <h3 className="login-title">Silakan Login</h3>
+
+            <form onSubmit={handleFormSubmit}>
+              <div>
+                <span
+                  className="text-center"
+                  style={{
+                    color: "red",
+                  }}
+                >
+                  {error}
+                </span>
+              </div>
+              <div className="form-group mb-3">
+                <label htmlFor="inputEmail" className="label-input">
+                  Email
+                </label>
+                <input
+                  id="inputEmail"
+                  type="email"
+                  placeholder="Email address"
+                  className="form-control px-4 input-form"
+                  value={credential}
+                  onChange={(event) => setCredential(event.target.value)}
+                />
+              </div>
+              <div className="form-group mb-3">
+                <label htmlFor="inputPassword" className="label-input">
+                  Password
+                </label>
+                <input
+                  id="inputPassword"
+                  type={typePassword}
+                  placeholder="Password"
+                  className="form-control px-4 input-form"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+                <span className="p-viewer2">
+                  <img
+                    src={`${process.env.PUBLIC_URL}/assets/img/${
+                      typePassword === "password" ? "Hide.png" : "view.png"
+                    }`}
+                    alt="eys"
+                    onClick={visiblePasswordChange}
+                  />
+                </span>
+              </div>
+              <div className="text-right d-flex justify-content-end mt-4">
+                <p className="forgot-text">
+                  Forgot
+                  <a
+                    href="https://bootstrapious.com/snippets"
+                    className="password-text"
+                  >
+                    {" "}
+                    Password?
+                  </a>
+                </p>
+              </div>
+              <div
+                className="d-flex justify-content-end"
+                style={{ marginRight: "0px" }}
+              >
+                <button type="submit" className="btn-login">
+                  {submitted ? "Loading..." : "Login"}
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
